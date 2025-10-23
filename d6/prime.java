72bdf0ce3b9f169763074c6a72dcb62f1a1fde1c
Blueye21 <danjdoh@gmail.com>
2025-10-22 17:12:54 -0700

    Day 5

---
+++ d6/prime.java
@@ -0,0 +1,27 @@
+package cohding.java.d6;
+import java.util.Scanner;
+
+public class prime {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        while (true) {
+            int n = scn.nextInt();
+            boolean prime = false;
+
+            for (int div = 2; div + div <= n; div++) {
+                if (n % div == 0) {
+                    prime = true;
+                    break;
+                }
+            }
+
+            if (prime) {
+                System.out.println("unprime");
+            } else {
+                System.out.println("prime");
+            }
+        }
+    }
+}
---
+++ d6/primegenerator.java
@@ -0,0 +1,25 @@
+package cohding.java.d6;
+import java.util.Scanner;
+
+public class primegenerator {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int low = scn.nextInt();
+        int high = scn.nextInt();
+
+        for (int n = low; n <= high; n++) {
+            boolean count = false;
+
+            for (int div = 2; div + div <= n; div++) {
+                if (n % div == 0) {
+                    count = true;
+                    break;
+                }
+            }
+
+            if (count == false && n > 1) {
+                System.out.println(n);
+            }
+        }
+    }
+}
